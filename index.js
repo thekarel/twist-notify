7b0f7ba38f95a77d37ecf9018300be00a09c1700
Charles Szilagyi <k@szil.me>
2023-10-13 11:01:06 +0100

    chore: Update

--- index.js
+++ index.js
@@ -3,16 +3,18 @@ const http = require('@actions/http-client');
 
 async function run() {
   try {
+    const client = new http.HttpClient('thekarel/twist-notify');
+
     const url = core.getInput('url');
+    console.log("url.length", url.length);
     const topic = core.getInput('topic');
     const message = core.getInput('message');
 
-    const client = new http.HttpClient('thekarel/twist-notify');
 
-    await client.post(url, JSON.stringify({
-      topic,
-      message
-    }));
+    const payload = JSON.stringify({ topic, message })
+    const additional = { headers: { 'Content-Type': 'application/json' } }
+
+    await client.post(url, payload, additional);
   } catch (error) {
     core.setFailed(error.message);
   }
